febe2ea1213e6d2c40703c9bdcf03b75c4826730
Rose <LeahYesX@gmail.com>
2023-08-28 16:02:49 -0400

    Add prefixes

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import responses
 from time import sleep
+import os
+import json
 
 def get_token():
     with open("data/bot/t.txt", "r") as f:
@@ -8,14 +10,39 @@ def get_token():
     
 TOKEN = get_token()
 
-async def send_message(message, user_message, channel_id, server_id):
+async def send_message(message, user_message, channel_id, server_id, prefix):
     try:
-        response = responses.handle(user_message, channel_id, server_id)
+        response = responses.handle(user_message, channel_id, server_id, prefix)
 
         await message.channel.send(response)
 
     except Exception as e:
-        print(e)
+        if str(e) == "400 Bad Request (error code: 50006): Cannot send an empty message":
+            print("No response found")
+        else:
+            print(e)
+
+async def get_prefix(server) -> str():
+    # Check if server has assigned prefix
+    if os.path.exists(f"data/server/{server}/prefix.txt") == False:
+        # If not then make server path, and create prefix from server_defaults.json
+        os.makedirs(name=f"data/server/{server}", exist_ok=True)
+
+        with open("data/client/server_defaults.json", "r") as f:
+            prefix = json.loads(f.read())["prefix"]
+        
+        with open(f"data/server/{server}/prefix.txt", "w") as f:
+            f.write(prefix)
+
+        return prefix
+        
+        
+    else:
+        # If it does exist then read prefix and return it.
+        with open(f"data/server/{server}/prefix.txt", "r") as f:
+            return f.read()
+
+
 
 
 def run():
@@ -46,9 +73,10 @@ def run():
         channel_id = str(message.channel.id)
         server_id = str(message.guild.id)
 
-        if user_message.startswith(f"{prefix}"):
-            print(f"{username} said: '{user_message}' in: '{server_id}:{channel_id}'")
+        prefix = await get_prefix(server_id)
 
-            await send_message(message,user_message,channel_id)
+        print(f"{username} said: '{user_message}' in: '{server_id}:{channel_id}'")
+
+        await send_message(message,user_message,channel_id,server_id,prefix)
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,4 +1,24 @@
 
-def handle(message:str, channel:str, server:str) -> str:
+
+def handle(message:str, channel:str, server:str, prefix:str) -> str:
     full_message = message
     message = message.lower()
+
+    if message == f"{prefix}example":
+        return "Example Message"
+    
+    elif message.startswith(f"{prefix}prefix"):
+        new_prefix = message.removeprefix(f"{prefix}prefix ")
+        if message == f"{prefix}prefix" or new_prefix.endswith(" "):
+            return f"# Prefix syntax\n{prefix}prefix <string>"
+        
+        else:
+            if new_prefix == prefix:
+                return f"Prefix is already *{prefix}*"
+            
+            else:
+                with open(f"data/server/{server}/prefix.txt", "w") as f:
+                    f.write(new_prefix)
+
+                print(f"Changed prefix in server: '{server}' to {new_prefix}")
+                return f"Changed prefix from: *{prefix}* to: *{new_prefix}*"
